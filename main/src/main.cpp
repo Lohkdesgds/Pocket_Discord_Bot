f0e7fbd611e8f9c1f34c76c131a18fdba9222731
Lohkdesgds <lohk.lohkdesgds@gmail.com>
2023-12-26 22:07:29 -0300

    Paired with pocket_discord development, simple testing there.

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -11,27 +11,35 @@ extern "C" { void app_main(void); }
 
 #include "discord/core.hpp"
 
+using namespace Lunaris;
+using namespace PocketDiscord;
 
 void app_main(void)
 {
-    const Lunaris::PocketDiscord::ram_info b4{};
+    const ram_info b4{};
 
-    Lunaris::PocketDiscord::Bot* bot /*= nullptr;*/= new Lunaris::PocketDiscord::Bot();
+    BotBase* bot /*= nullptr;*/= new Lunaris::PocketDiscord::BotBase();
 
     //vTaskDelay(pdMS_TO_TICKS(10000));
 
     /*for(size_t p = 0; p < 10; ++p) {
         ESP_LOGI("MAIN", "================================ ITERATION %zu OF 10 ================================", p + 1);
-        bot = new Lunaris::PocketDiscord::Bot();
+        bot = new BotBase();
         vTaskDelay(pdMS_TO_TICKS(1000));
         delete bot;
         ESP_LOGI("MAIN", "================================ END OF ITERATION %zu OF 10 ================================", p + 1);
         vTaskDelay(pdMS_TO_TICKS(1000));
     }*/
+
+    {
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        auto thebot = bot->make_bot("TOKEN HERE", gateway_intents::NONE, nullptr);
+        vTaskDelay(pdMS_TO_TICKS(20000));
+    }
     
     delete bot;
     
-    const Lunaris::PocketDiscord::ram_info af{};
+    const ram_info af{};
         
     ESP_LOGI("MAIN", 
         "Memory usage before:\n"
@@ -52,7 +60,7 @@ void app_main(void)
     while(1) {
         vTaskDelay(pdMS_TO_TICKS(10000));
         
-        const Lunaris::PocketDiscord::ram_info af2{};
+        const ram_info af2{};
         
         ESP_LOGI("MAIN", 
             "Memory usage recurrent:\n"
